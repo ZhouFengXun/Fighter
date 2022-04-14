2b6ff5d1c5395c7ad35126f0c05ce2bde78c04ab
zhoufengxun <2281461277@qq.com>
2022-04-14 10:49:27 +0800

    获取容器个数 容器名

--- fighter-aop/src/main/java/com/fanlan/fighteraop/FighterAopApplication.java
+++ fighter-aop/src/main/java/com/fanlan/fighteraop/FighterAopApplication.java
@@ -2,12 +2,24 @@ package com.fanlan.fighteraop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FighterAopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FighterAopApplication.class, args);
+        //获取容器
+        ApplicationContext applicationContext = SpringApplication.run(FighterAopApplication.class, args);
+        //获取容器的数量
+        int beanDefinitionCount = applicationContext.getBeanDefinitionCount();
+        System.out.println(beanDefinitionCount);
+        //获取bean名称
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
+
+
     }
 
 }
